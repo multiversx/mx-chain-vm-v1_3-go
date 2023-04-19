d5ac520c1f7ef82c3d7dee1d6ed75b0881afc866
BeniaminDrasovean <beniamin.drasovean@elrond.network>
2023-04-19 16:57:18 +0300

    update vm common

--- mock/world/worldAccount.go
+++ mock/world/worldAccount.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"math/big"
 
-	"github.com/multiversx/mx-chain-core-go/core"
 	logger "github.com/multiversx/mx-chain-logger-go"
 	"github.com/multiversx/mx-chain-vm-common-go"
 	"github.com/multiversx/mx-chain-vm-v1_3-go/crypto/hashing"
@@ -286,6 +285,6 @@ func cloneBytes(b []byte) []byte {
 }
 
 // MigrateDataTrieLeaves -
-func (a *Account) MigrateDataTrieLeaves(_ core.TrieNodeVersion, _ core.TrieNodeVersion, _ vmcommon.DataTrieMigrator) error {
+func (a *Account) MigrateDataTrieLeaves(_ vmcommon.ArgsMigrateDataTrieLeaves) error {
 	return nil
 }
